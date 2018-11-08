8e770105b16ad9c4c25f1467400d387cd0046fb7
vojtatravnicek <vojta.travnicek@seznam.cz>
2018-11-08 08:56:25 +0100

    Zmena - na X

--- Pyramida.java
+++ Pyramida.java
@@ -5,7 +5,7 @@ public class Pyramida {
 		
 		for (int y = 0; y < vyska; y++) {
 			for (int x = 0; x < y * 2 + 1; x++) {
-				System.out.printf("-");
+				System.out.printf("X");
 			}
 			System.out.println();
 		}

498628f1b6755bde62ff5324bf0c95d48b7c0b13
vojtatravnicek <vojta.travnicek@seznam.cz>
2018-11-08 09:02:57 +0100

    Tiskneme pyramidu

--- Pyramida.java
+++ Pyramida.java
@@ -1,13 +1,15 @@
-/* Nakresli (krasnou) pyramidu. */
 public class Pyramida {
 	public static void main(String[] args) {
 		int vyska = Integer.parseInt(args[0]);
 		
 		for (int y = 0; y < vyska; y++) {
+			for (int z = 0; z < vyska - y - 1; ++z) {
+				System.out.printf (" ");
+			}
 			for (int x = 0; x < y * 2 + 1; x++) {
-				System.out.printf("X");
+				System.out.printf ("X");
 			}
 			System.out.println();
-		}
+		}	
 	}
-}
+}	
